7ed8167afad959c155fb8d7cd29198dda4ab14a8
贾旭光 <2860905270@qq.com>
2019-11-13 22:32:57 +0800

    黑马头条首页02

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="login">
-    <el-card class="login-card"></el-card>
+    <el-card class="login-card">
+       <div class="title">
+            <img src="../../assets/logo_index.png" alt="">
+        </div>
+            <el-form style="margin-top:20px">
+                <el-form-item>
+                 <el-input placeholder="请输入手机号"></el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-input placeholder="请输入验证码" style="width:240px"></el-input>
+                  <el-button style="float:right" plain>发送验证码</el-button>
+                </el-form-item>
+                <el-form-item>
+                    <!-- 复选框 -->
+                      <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+                </el-form-item>
+                <el-form-item  style="text-align:center">
+                     <el-button type="primary" style="width:100%">登录</el-button>
+                 </el-form-item>
+            </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -20,6 +40,13 @@ export default {}
   .login-card {
     width: 400px;
     height: 300px;
+    padding-bottom: 40px;
+    .title {
+        text-align: center;
+        img {
+            height: 45px;
+        }
+    }
   }
 }
 </style>
